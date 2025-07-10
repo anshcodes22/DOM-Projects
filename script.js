dbba41fc9d6c7414fde91cdda285338bf690da97
Ansh <puri.ansh05@gmail.com>
2025-07-10 23:14:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('Hello!');
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('.canvas');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+        console.log(event.target);
+        
+        if (event.target.id === 'grey') {
+            body.style.backgroundColor = event.target.id;
+        } else if (event.target.id === 'white') {
+            body.style.backgroundColor = event.target.id;
+        } else if (event.target.id === 'blue') {
+            body.style.backgroundColor = event.target.id;
+        } else if (event.target.id === 'pink') {
+            body.style.backgroundColor = event.target.id;
+        } 
+    })
+});
